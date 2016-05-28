a0ab0d0b015ec0c2de06155938417a7b06e6d072
Radosław Kapłon <radziuk96@gmail.com>
2016-05-28 19:15:12 +0200

    Dodanie funkcji logowania w osobnym projekcie, utworzenie bazy danych na Azure i zintegrowanie jej

---
+++ ConferenceAndAuthentication/conffandauthh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(conffandauthh.Startup))]
+namespace conffandauthh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
