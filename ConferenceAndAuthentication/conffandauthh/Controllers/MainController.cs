80912bb1c9f7fdbc13b241cf1e022152ff3d9c70
Radosław Kapłon <radziuk96@gmail.com>
2016-05-30 19:29:01 +0200

    Pobieranie aktualnie zalogowanego uzytkownika z bazy za pomocą MainController. Wyświetlenie jego maila w czacie

---
+++ ConferenceAndAuthentication/conffandauthh/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using conffandauthh.Models;
+using System.Web.Mvc;
+
+namespace conffandauthh.Controllers
+{
+    public class MainController : Controller
+    {
+        protected ApplicationUser getUser()
+        {
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            return manager.FindById(User.Identity.GetUserId());
+            
+        }
+    }
+}
