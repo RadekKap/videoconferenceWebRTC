0b644379c7380b55ec05a68aa2cf3c55bce6606b
Mikołaj Żołędziowski <frost1111@wp.pl>
2016-05-31 16:34:15 +0200

    Dodanie controlera History

---
+++ ConferenceAndAuthentication/conffandauthh/Controllers/HistoryController.cs
@@ -0,0 +1,37 @@
+﻿using conffandauthh.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace conffandauthh.Controllers
+{
+    public class HistoryController : Controller
+    {
+        // GET: History
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public void Add(HistoryModel History)
+        {
+            using (var db = new conferenceEntities2())
+            {
+                var his = db.Set<ChatHistory>();
+                var roomid = db.Rooms.First(r => r.name == History.roomname).roomId;
+
+
+                ChatHistory chat = new ChatHistory { userId = User.Identity.GetUserId(), oldRoomId = roomid, content = History.content };
+                his.Add(chat);
+                db.SaveChanges();
+
+            }
+        }
+
+
+    }
+}
---
+++ ConferenceAndAuthentication/conffandauthh/Models/HistoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace conffandauthh.Models
+{
+    public class HistoryModel
+    {
+
+        public int messageId;
+        public int oldRoomId;
+        public string userId;
+        public string content;
+        public string roomname;
+    }
+}
