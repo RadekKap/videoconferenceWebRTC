16eb7aaf9568655cf83bce3790ecc53ab37950f6
Radosław Kapłon <radziuk96@gmail.com>
2016-06-01 11:59:16 +0200

    dodanie funkcji wyszukiwania i zapraszania znajomych w zakładce 'Znajomi'

--- ConferenceAndAuthentication/conffandauthh/Controllers/FriendsController.cs
+++ ConferenceAndAuthentication/conffandauthh/Controllers/FriendsController.cs
@@ -8,12 +8,90 @@ using System.Web.Mvc;
 
 namespace conffandauthh.Controllers
 {
-    public class FriendsController : Controller
+    public class FriendsController : MainController
     {
+        static List<string> ListF = new List<string>();
+        ApplicationDbContext dbcont = new ApplicationDbContext();
+        conferenceEntities2 db = new conferenceEntities2();
+
         // GET: Friends
         public ActionResult Index()
         {
-            return View();
+            SearchFriendModel model = new SearchFriendModel();
+            var allusers = dbcont.Users.ToArray();
+            
+
+            ApplicationUser user = getUser();
+            int pom = 0;
+
+            var find = from docs in db.Invitation where docs.firstUserId == user.Id select docs;
+
+            var invitations = find.ToArray();
+
+            model.ListFriends = new string[allusers.Count() - 1 - invitations.Count()];
+
+            foreach (var x in allusers)
+            {
+                if (!x.Email.Equals(user.Email.ToString()))
+                {
+                    foreach (var inv in invitations)
+                    {
+                        if (!inv.secondUserId.Equals(x.Id))
+                        {
+                            model.ListFriends.SetValue(x.Email, pom);
+                            pom++;
+                        }
+                    }
+                }
+
+            }
+
+            
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(SearchFriendModel F)
+        {
+
+           string[] tab= new string[F.ListFriends.Count() - 1];
+
+            int y = 0;
+            foreach (var x in F.ListFriends)
+            {
+                if (!x.Equals(F.Email))
+                {
+                   tab.SetValue(x, y);
+                    y++;
+                }
+
+            }
+            F.ListFriends = tab;
+
+            ApplicationUser user = getUser();
+            var allusers = dbcont.Users.ToArray();
+            var inviteeID=allusers[0].Id;
+            
+            foreach (var x in allusers)
+            {
+                if (x.Email.Equals(F.Email))
+                {
+                     inviteeID = x.Id;
+                }
+
+            }
+
+            Invitation invitetoAdd = new Invitation
+            {
+                firstUserId = user.Id,
+                secondUserId = inviteeID
+            };
+
+            db.Invitation.Add(invitetoAdd);
+            db.SaveChanges();
+
+            return View(F);
         }
 
         public string test()
---
+++ ConferenceAndAuthentication/conffandauthh/Models/SearchFriendModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace conffandauthh.Models
+{
+    public class SearchFriendModel
+    {
+        public string Email { get; set; }
+
+        public string[] ListFriends { get; set; }
+    }
+}
